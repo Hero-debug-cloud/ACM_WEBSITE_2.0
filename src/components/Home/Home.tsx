782d85df47bbae86ba0e2437ae4a4305a3b3262e
vinay <vinaychandola@gmail.com>
2023-05-27 23:36:01 +0530

    updating

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,13 +2,24 @@ import styles from "./style.module.css"
 import Acm_front from "../Acm_front/Acm_front";
 import Slider_comp from "../Slider_comp/Slider_comp"
 import About from "../../components/About/About";
+import About_info from "../About_info/About_info";
+import info from "../../../db/info.json";
 
 const Home = () => {
+  type valuetype={
+    id:number,
+    heading:string,
+    dis:string,
+    position:string
+  }
   return (
     <div className={styles.home}>
       <Acm_front/>
       <Slider_comp/>
       <About/>
+      {info.map((value:valuetype)=>{
+        return <About_info id={value.id} img="something" heading={value.heading} dis={value.dis} position={value.position}/>
+      })}
     </div>
   )
 }
